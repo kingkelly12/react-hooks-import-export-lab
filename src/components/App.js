973a3885cf1500699596e8e9e7c48504eeeec6cc
Kelly Koome <koomekelly7@gmail.com>
2025-04-14 10:41:59 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import { image } from "../data/user";
+
 
 function App() {
   return (
